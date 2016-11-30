52985351e20239aaf7d16efe3f2a959489720324
udaypb <uday.pb26@gmail.com>
2016-11-30 12:08:09 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -21,12 +21,7 @@ $(document).ready(function(){
   //------------------------------------//
   //Scroll To//
   //------------------------------------//
-  $(".scroll").click(function(event){		
-  	event.preventDefault();
-  	$('html,body').animate({scrollTop:$(this.hash).offset().top}, 800);
-  	
-  });
-  
+ 
   //------------------------------------//
   //Wow Animation//
   //------------------------------------// 
